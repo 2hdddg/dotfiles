aaedc77e7701e8185344c342812be8ed897d72f9
Peter Wilhelmsson <peterw@axis.com>
2026-08-11 14:36:15 +0200

    Added hop

--- nvim/init.lua
+++ nvim/init.lua
@@ -57,8 +57,8 @@ vim.pack.add({
     'https://github.com/2hdddg/toggleTerm.nvim',
     -- File explorer
     'https://github.com/stevearc/oil.nvim',
-    -- Copilot
-    --'https://github.com/github/copilot.vim.git'
+    -- Faster navigation
+    'https://github.com/smoka7/hop.nvim',
 })
 
 require('mini.icons').setup({})
@@ -79,6 +79,7 @@ vim.api.nvim_set_hl(0, "StatusLine", {reverse = false})
 vim.api.nvim_set_hl(0, "StatusLineNC", {reverse = false})
 require('statusline') -- Must be after highlights
 
+require('hop').setup()
 
 -- ==========================
 -- Keymaps
@@ -169,6 +170,26 @@ vim.keymap.set({'i','c'}, '<CR>', function() return key_if_pum_visible('<C-y>', 
 vim.keymap.set('i', '(', function() return key_if_pum_visible('<C-y>(', '(') end, replace_keycodes)
 -- Close popup menu with esc
 vim.keymap.set({'i','c'}, '<esc>', function() return key_if_pum_visible('<C-e>', '<esc>') end, replace_keycodes)
+-- Hop
+local hop = require('hop')
+local directions = require('hop.hint').HintDirection
+vim.keymap.set('', 'f', function()
+  hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true })
+end, {remap=true})
+vim.keymap.set('', 'F', function()
+  hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true })
+end, {remap=true})
+vim.keymap.set('', 't', function()
+  hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true, hint_offset = -1 })
+end, {remap=true})
+vim.keymap.set('', 'T', function()
+  hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true, hint_offset = 1 })
+end, {remap=true})
+vim.keymap.set("n", "<leader>h", function()
+  hop.hint_words({
+      multi_windows = true,
+  })
+end, {remap=true})
 
 -- Setup treesitter to use highlighting
 require('nvim-treesitter').setup({
