2e9fa52eb527c3eba79e5889132fa1919b41c281
Peter Wilhelmsson <peterw@axis.com>
2026-08-12 13:32:47 +0200

    Skip hop with f/t, use leader/leader for hop

--- nvim/init.lua
+++ nvim/init.lua
@@ -173,19 +173,7 @@ vim.keymap.set({'i','c'}, '<esc>', function() return key_if_pum_visible('<C-e>',
 -- Hop
 local hop = require('hop')
 local directions = require('hop.hint').HintDirection
-vim.keymap.set('', 'f', function()
-  hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true })
-end, {remap=true})
-vim.keymap.set('', 'F', function()
-  hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true })
-end, {remap=true})
-vim.keymap.set('', 't', function()
-  hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true, hint_offset = -1 })
-end, {remap=true})
-vim.keymap.set('', 'T', function()
-  hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true, hint_offset = 1 })
-end, {remap=true})
-vim.keymap.set("n", "<leader>h", function()
+vim.keymap.set("n", "<leader><leader>", function()
   hop.hint_words({
       multi_windows = true,
   })
